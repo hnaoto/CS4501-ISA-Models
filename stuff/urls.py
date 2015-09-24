b44f5421c4448d09cdd8bbbe3cc5299a852ce0ca
hnaoto <hnaoto@me.com>
2015-09-24 19:48:49 -0400

    add admin site

--- stuff/urls.py
+++ stuff/urls.py
@@ -5,11 +5,15 @@ from stuff import main
 
 urlpatterns = patterns('',
                        
+                       
+                       
+                       url(r'^admin/', include(admin.site.urls)),
                        url(r'^api/v1/users/create$', main.create_user),
                        url(r'^api/v1/transaction/create$', main.create_transaction),
                        url(r'^api/v1/company/create$', main.create_company),
                        url(r'^api/v1/job-application/create$', main.create_JobApplication),
                        url(r'^api/v1/users/(\d+)$', main.lookup_user),
+                       
                        
 
 )
