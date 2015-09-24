e2c217186db19e88fe5d631b5dc15f855ae7cd27
hnaoto <hnaoto@me.com>
2015-09-24 19:51:19 -0400

    add admin site

--- stuff/settings.py
+++ stuff/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-                      #'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
                       #'django.middleware.csrf.CsrfViewMiddleware',
-                      #'django.contrib.auth.middleware.AuthenticationMiddleware',
-                      #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
                       #'django.contrib.messages.middleware.MessageMiddleware',
                       #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
