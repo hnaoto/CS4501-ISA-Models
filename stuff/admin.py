f3e740baa9c45b33f70468b5b5d8f8427dafa909
hnaoto <hnaoto@me.com>
2015-09-24 20:22:05 -0400

    add admin site

--- stuff/admin.py
+++ stuff/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from stuff.models import User
+from stuff.models import User, Company, Buyer, Seller, Transaction, JobApplication
 
 admin.site.register(User)
+admin.site.register(Seller)
+admin.site.register(Buyer)
+admin.site.register(Company)
+admin.site.register(Transaction)
+admin.site.register(JobApplication)
