03a876454c845c36c5b7a334059d41a269a15064
hnaoto <hnaoto@me.com>
2015-10-01 20:05:10 -0400

    turned on MessageMiddleware

--- stuff/settings.py
+++ stuff/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE_CLASSES = (
                       #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-                      #'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
                       #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
